319e3475d7b8c39889443026c71aea8efa5da150
Lcs-Knsk <84292789+Lcs-Knsk@users.noreply.github.com>
2022-08-04 00:50:27 -0400

    Sign up and login are on same page

--- website/auth.py
+++ website/auth.py
@@ -10,49 +10,46 @@ auth = Blueprint('auth', __name__)
 @auth.route("/sign_up", methods=['GET', 'POST'])
 def SignUp():
      if request.method == 'POST':
-          UserName = request.form.get('Username')
-          password1 = request.form.get('Password')
-          password2 = request.form.get('Password2')
 
-          user = User.query.filter_by(username=UserName).first()
-          if user:
-               flash('Username already exists.', category='error')
-          elif len(UserName) < 4:
-               flash('First name must be greater than 3 characters.', category='error')
-          elif password1 != password2:
-               flash('Passwords don\'t match.', category='error')
-          elif len(password1) < 7:
-               flash('Password must be at least 7 characters.', category='error')
-          else:
-               new_user = User(username=UserName, password=generate_password_hash(password1, method='sha256'))
-               db.session.add(new_user)
-               db.session.commit()
-               flash('Account created!', category='success')
-               login_user(user, remeber=True)
-               return redirect(url_for('views.Home'))
+          if "signUpForm" in request.form:
+               UserName = request.form.get('Username')
+               password1 = request.form.get('Password')
+               password2 = request.form.get('Password2')
 
-
-     return render_template("SignIn.html", test="Hello Flask", testCase=True)
-
-@auth.route("/login", methods=['GET', 'POST'])
-def Login():
-     if request.method == 'POST':
-          username = request.form.get('Username')
-          password = request.form.get('Password')
-
-          user = User.query.filter_by(username=username).first()
-          if user: 
-               if check_password_hash(user.password, password):
-                    flash('Logged in', category='success')
-                    login_user(user, remember=True)
-                    return redirect(url_for('views.Home'))
-
+               user = User.query.filter_by(username=UserName).first()
+               if user:
+                    flash('Username already exists.', category='error')
+               elif len(UserName) < 4:
+                    flash('First name must be greater than 3 characters.', category='error')
+               elif password1 != password2:
+                    flash('Passwords don\'t match.', category='error')
+               elif len(password1) < 7:
+                    flash('Password must be at least 7 characters.', category='error')
                else:
-                    flash('Incorrect password.', category='error')
-          else:
-               flash('Username does not exist.', category='error')
-     
-     return render_template("Login.html")
+                    new_user = User(username=UserName, password=generate_password_hash(password1, method='sha256'))
+                    db.session.add(new_user)
+                    db.session.commit()
+                    flash('Account created!', category='success')
+                    login_user(new_user, remember=True)
+                    return redirect(url_for('views.Home'))
+         
+          elif "loginForm" in request.form:
+               username = request.form.get('Username')
+               password = request.form.get('Password')
+
+               user = User.query.filter_by(username=username).first()
+               if user: 
+                    if check_password_hash(user.password, password): 
+                         login_user(user, remember=True)
+                         return redirect(url_for('views.Home'))
+                    else:
+                         flash('Incorrect password.', category='error')
+               else:
+                    flash('Username does not exist.', category='error')
+
+
+     return render_template("SignUp.html", test="Hello Flask", testCase=True)
+
 
 @auth.route('/logout')
 @login_required
